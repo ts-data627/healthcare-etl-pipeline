3655fc6b95b01905279747b44b87e250c58adb59
Codingchiro31 <139086942+Codingchiro31@users.noreply.github.com>
2026-02-21 21:10:30 -0600

    Add load.py placeholder

---
+++ load.py
@@ -0,0 +1,14 @@
+
+"""
+load.py
+Load data to PostgreSQL database
+"""
+
+import psycopg2
+
+def load_data(clean_data):
+  """Load data to RDS PostgreSQL"""
+  pass
+
+if __name__ == "__main__":
+  load_data()
